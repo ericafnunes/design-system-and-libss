8443e7a595a56b89db3637de91ab81a03f71f2f6
Erica <eriica.f.n@hotmail.com>
2022-04-01 14:09:48 -0300

    alterando projeto

--- src/components/CustomAlert/index.js
+++ src/components/CustomAlert/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Alert from '@material-ui/lab/Alert';
-import useStyles from './style';
+import React from "react";
+import Alert from "@material-ui/lab/Alert";
+import useStyles from "./style";
 
 export default function CustomAlert() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <Alert severity="error">Pokemon não encontrado!</Alert>
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <Alert severity="error">Pokemon não encontrado!!!</Alert>
+    </div>
+  );
 }
